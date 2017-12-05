191dc9118fd10903c155d16dcf181bfc7ad25f12
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-12-05 06:42:12 +0100

    Update

--- app/GroupePrice.php
+++ app/GroupePrice.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class GroupePrice extends Model
 {
-    //
+    public $incrementing = false; //or should it be true??
+    public $timestamps = false;
+
+    // whitelisting all table fields
+    protected $fillable = [
+        'id',
+        'price',
+        'group_id',
+        'price_id',
+    ];
 }
--- database/migrations/2017_11_21_130052_create_customers_table.php
+++ database/migrations/2017_11_21_130052_create_customers_table.php
@@ -26,12 +26,11 @@ class CreateCustomersTable extends Migration
             $table->integer('default_billing')->nullable();
             $table->integer('default_shipping')->nullable();
             $table->integer('is_active')->nullable();
-            $table->date('created_at')->nullable();
-            $table->date('updated_at')->nullable();
             $table->string('customer_invoice_email')->nullable();
             $table->text('customer_extra_text')->nullable();
             $table->integer('customer_due_date_period', false, true)->nullable();
             $table->integer('address_id', false, true)->nullable();
+            $table->timestamps();
         });
     }
 
